d9f80180baf411e15b5f3a134ce52ec4d66ebfef
pinale <pinale@users.noreply.github.com>
2019-07-25 21:03:33 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 
+import { DynamicFormComponent } from './dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent,DynamicFormComponent, DynamicFormQuestionComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
